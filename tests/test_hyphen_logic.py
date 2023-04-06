45aa361b428417ad27775a9c9c8eb56f146eba9a
CangyuanLi <everest229@gmail.com>
2023-04-06 16:03:19 -0400

    reformat tests

--- tests/test_hyphen_logic.py
+++ tests/test_hyphen_logic.py
@@ -2,32 +2,21 @@ import pytest
 
 from title_caser import ChicagoStyler
 
-TITLE1 = "Under-the-Counter Transactions and Out-of-Fashion Initiatives"
-TITLE2 = "Bed-and-Breakfast Options in Upstate New York"
-TITLE3 = "Record-Breaking Borrowings from Medium-Sized Libraries"
-TITLE4 = "Cross-Stitching for Beginners"
-TITLE5 = "A History of the Chicago Lying-In Hospital"
-TITLE6 = "The E-flat Concerto"
-TITLE7 = "Self-Sustaining Reactions"
-TITLE8 = "Anti-intellectual Pursuits"
-TITLE9 = "Does E-mail Alter Thinking Patterns?"
-TITLE10 = "A Two-Thirds Majority of Non-English-Speaking Representatives"
-TITLE11 = "Ninety-Fifth Avenue Blues"
-TITLE12 = "Atari's Twenty-First-Century Adherents"
-
 TITLES = [
-    TITLE1,
-    TITLE2,
-    TITLE3,
-    TITLE4,
-    TITLE5,
-    TITLE6,
-    TITLE6,
-    TITLE8,
-    TITLE9,
-    TITLE10,
-    TITLE11,
-    TITLE12,
+    "Under-the-Counter Transactions and Out-of-Fashion Initiatives",
+    "Bed-and-Breakfast Options in Upstate New York",
+    "Record-Breaking Borrowings from Medium-Sized Libraries",
+    "Cross-Stitching for Beginners",
+    "A History of the Chicago Lying-In Hospital",
+    "The E-flat Concerto",
+    "Self-Sustaining Reactions",
+    "Anti-intellectual Pursuits",
+    "Does E-mail Alter Thinking Patterns?",
+    "A Two-Thirds Majority of Non-English-Speaking Representatives",
+    "Ninety-Fifth Avenue Blues",
+    "Atari's Twenty-First-Century Adherents",
+    "Are We All a Little Stalker-ish?",
+    "Organic Tomato-y Pasta with Vegetables",
 ]
 
 
